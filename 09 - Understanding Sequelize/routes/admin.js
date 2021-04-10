90e572f0bc4d9cbc71f691b5ede834e82764f1d3
rawallon <rawallon@gmail.com>
2021-04-10 10:32:05 -0300

    Added ninth module

---
+++ 09 - Understanding Sequelize/routes/admin.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express = require('express');
+
+const {
+  getAddProduct,
+  getProducts,
+  postAddProduct,
+  getEditProduct,
+  postEditProduct,
+  postDeleteProduct,
+} = require('../controllers/admin');
+
+const router = express.Router();
+
+router.get('/add-product', getAddProduct);
+
+router.get('/products', getProducts);
+
+router.post('/add-product', postAddProduct);
+
+router.get('/edit-product/:productId', getEditProduct);
+
+router.post('/edit-product', postEditProduct);
+
+router.post('/delete-product', postDeleteProduct);
+
+module.exports = router;
